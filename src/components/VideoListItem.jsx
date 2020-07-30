fcded9861a2d1cdf845b9334cdf0fed1b2ddc2b9
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-30 23:42:19 +0100

    render video list item

---
+++ src/components/VideoListItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const VideoListItem = (props) => {
+  const { video, onVideoSelect, selectedVideo } = props;
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  const isSelected = selectedVideo === video;
+
+  return (
+    <li
+      className={`list-group-item ${isSelected ? "selected" : ""}`}
+      key={video.etag}
+      onClick={() => onVideoSelect(video)}
+    >
+      <div className="video-list media">
+        <div className="media-left">
+          <img alt="preview" src={imageUrl} className="video-item-img" />
+        </div>
+        <div className="media-body">
+          <div className="media-heading ml-5">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
